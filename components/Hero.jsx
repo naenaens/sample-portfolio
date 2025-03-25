d9c753c97e58863d116673a40d9fcc2b8a908dae
Nina Claire Saberon <rivacaninaclaire@gmail.com>
2025-03-25 09:01:20 -0600

    hero again

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -16,9 +16,9 @@ import Socials from './Socials';
 
 const Hero = () => {
   return (
-    <section className='py-12 xl:py-24 h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none'>
+    <section className='py-12 xl:py-24 h-auto xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none mb-16'>
       <div className='container mx-auto'>
-        <div className='flex flex-col xl:flex-row justify-between gap-x-8'>
+        <div className='flex flex-col xl:flex-row justify-around gap-x-8'>
           {/* text */}
           <div className='flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left'>
             <div className='text-sm uppercase font-semibold mb-4 text-purple-800 dark:text-amber-400 tracking-[4px]'>
@@ -43,18 +43,10 @@ const Hero = () => {
             /> */}
           </div>
           
-          {/* image */}
-          <div className='flex justify-center xl:hidden mb-8'>
+          {/* image (no background) */}
+          <div className='flex justify-center xl:flex mb-8'>
             <DevImg
-              containerStyles='w-[350px] h-[500px] bg-no-repeat relative bg-bottom'
-              imgSrc='/hero/developer.png'
-            />
-          </div>
-          
-          <div className='hidden xl:flex relative'>
-            <div className='bg-amber-400 w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2'></div>
-            <DevImg
-              containerStyles='bg-amber-400 w-[350px] h-[500px] bg-no-repeat relative bg-bottom'
+              containerStyles='w-[450px] h-[650px] bg-no-repeat relative bg-bottom'
               imgSrc='/hero/developer.png'
             />
           </div>
